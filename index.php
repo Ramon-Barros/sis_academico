d2cdb2d6f715aaf46cbe30f39fec836cb7787e57
Ramon Barros <ramonfilipe@gmail.com>
2019-06-19 12:28:28 -0300

    criado o menu para rotas no sistema

--- index.php
+++ index.php
@@ -1,5 +1,21 @@
-<?php
-
-echo "BEM vindo aqui";
-
-?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Menu</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+    <b>MENU</b>
+    <ul>
+        <li><a class="nav-link active" href="index.php">Inicio</a></li>
+        <li><a class="nav-link active" href="cadastrodealuno.php">Aluno(a)</a></li>
+        <li><a class="nav-link active" href="cadastrodeprofessor.php">Professor(a)</a></li>
+        <li><a class="nav-link active" href="cadastrodocurso.php">Curso</a></li>
+        
+    </ul>
+</body>
+</html>

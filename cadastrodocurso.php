0957052be7e15486b3726758d87824c676ea6dcb
Ramon Barros <ramonfilipe@gmail.com>
2019-06-19 12:01:52 -0300

    página de cadastro terminada

--- cadastrodocurso.php
+++ cadastrodocurso.php
@@ -16,7 +16,7 @@
             <label for="aluno"> Nome:</label>
             <input type="text" id="aluno" name="aluno"><br>
             <label for="duracao"> Duração:</label>
-            <input type="text" id="duracao" name="duracao">meses<br>
+            <input type="text" id="duracao" name="duracao">  meses<br>
             <label for="coordenador">Selecione coordenador:</label>
                 <select name="coordenador" id="select">
                     <?php 
@@ -34,6 +34,31 @@
                            
                     ?>
                     </select>
+            <br>
+
+            <label for="nivel">Nível:</label>
+                <select name="nivel" id="select">
+                            <option value="">Selecione o nível do curso</option>
+                            <option value="tecnico">Técnico</option>
+                            <option value="tecnologo">Tecnólogo</option>
+                            <option value="bacharelado">Bacharelado</option>
+                            <option value="licenciatura">Licenciatura</option>
+                            <option value="especializacao">Especialização</option>
+                </select>
+
+            <br>
+
+            <label for="modalidade">Modalidade: </label>
+                <select name="modalidade" id="select">
+                    <option value="">Selecione a modalidade do curso</option>
+                    <option value="presencial">Presencial</option>
+                    <option value="distancia">À distância</option>
+                </select>
+
+            <br>
+            <br>
+
+
             <input type="reset" value="Limpar" name="limpar">
             <input type="submit" value="Enviar" name="submit"><br>
         </form>
